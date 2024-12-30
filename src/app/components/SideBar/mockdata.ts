59de0169350df39c852380bb2adef2b331209943
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-12-30 16:53:44 +0400

    feat: sidebar content added

---
+++ src/app/components/SideBar/mockdata.ts
@@ -0,0 +1,27 @@
+export const navs = [
+  {
+    title: "Home",
+    url: "home",
+    icon: ""
+  },
+  {
+    title: "Forums",
+    url: "forums",
+    icon: ""
+  },
+  {
+    title: "Resources",
+    url: "resources",
+    icon: ""
+  },
+  {
+    title: "",
+    url: "",
+    icon: ""
+  },
+  {
+    title: "",
+    url: "",
+    icon: ""
+  }
+];
--- src/app/components/SideBar/page.tsx
+++ src/app/components/SideBar/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { navs } from "./mockdata";
 
 type Props = {};
 
 function SideBar({}: Props) {
-  return <div>SideBar</div>;
+  return (
+    <div className="SideBar">
+      <div className="logo">Lectury</div>
+      <div className="navs">
+        {navs.map((nav) => (
+          <div className="nav">{nav.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
