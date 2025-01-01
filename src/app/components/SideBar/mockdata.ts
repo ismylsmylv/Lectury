71735c06dcecf98c12e289a7245dc2f31ab54274
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-01 11:00:27 +0400

    feat: sidebar links updated

--- src/app/components/SideBar/mockdata.ts
+++ src/app/components/SideBar/mockdata.ts
@@ -1,22 +1,34 @@
+import { RiHome5Fill } from "react-icons/ri";
+import { RiHome5Line } from "react-icons/ri";
+import { MdForum } from "react-icons/md";
+import { MdOutlineForum } from "react-icons/md";
+import { MdPermMedia } from "react-icons/md";
+import { MdOutlinePermMedia } from "react-icons/md";
+import { MdGroups } from "react-icons/md";
+import { MdOutlineGroups } from "react-icons/md";
 export const navs = [
   {
     title: "Home",
     url: "home",
-    icon: ""
+    iconFilled: RiHome5Fill,
+    iconOutline: RiHome5Line
   },
   {
     title: "Forums",
     url: "forums",
-    icon: ""
+    iconFilled: MdForum,
+    iconOutline: MdOutlineForum
   },
   {
     title: "Resources",
     url: "resources",
-    icon: ""
+    iconFilled: MdPermMedia,
+    iconOutline: MdOutlinePermMedia
   },
   {
     title: "Groups",
-    url: "",
-    icon: ""
+    url: "groups",
+    iconFilled: MdGroups,
+    iconOutline: MdOutlineGroups
   }
 ];
