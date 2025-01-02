6ecc3a2b64d7cf368acdf6c1e10c3477c7145dcb
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-02 20:34:17 +0400

    style: counts added && style fixes

--- src/app/components/SideBar/page.tsx
+++ src/app/components/SideBar/page.tsx
@@ -1,12 +1,11 @@
 "use client";
-import React, { useState } from "react";
-import { navs } from "./mockdata";
-import { v4 as uuidv4 } from "uuid";
-import "./style.scss";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
 import LogoImg from "@/app/assets/img/logo_banner_transparent.svg";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { navs } from "./mockdata";
+import "./style.scss";
 type Props = {};
 
 function SideBar({}: Props) {
@@ -14,23 +13,27 @@ function SideBar({}: Props) {
 
   return (
     <div className="SideBar">
-      {pathname}
       <Image alt="logo" src={LogoImg} className="logo" />
-      <div className="navs">
+      <div className="navs w-full">
         {navs.map((nav) => (
           <Link
             href={nav.url}
-            className={`nav flex items-center justify-start gap-4 mb-2 cursor-pointer p-4 pr-10 w-full rounded ${
+            className={`nav flex items-center justify-between mb-2 cursor-pointer p-4 pr-5 w-full rounded ${
               pathname === `${nav.url}` && "active"
             }`}
             key={uuidv4()}
           >
-            {pathname === `${nav.url}` ? (
-              <nav.iconFilled size={25} />
-            ) : (
-              <nav.iconOutline size={25} />
-            )}
-            {nav.title}
+            <div className="flex justify-start gap-4 ">
+              {pathname === `${nav.url}` ? (
+                <nav.iconFilled size={25} />
+              ) : (
+                <nav.iconOutline size={25} />
+              )}
+              {nav.title}
+            </div>
+            <div className="count flex items-center justify-center rounded-full">
+              9
+            </div>
           </Link>
         ))}
       </div>
