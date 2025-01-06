10acce2303c1f1a0c94138234755b2ad08845043
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-06 09:19:50 +0400

    style: breadcrumbs updated

--- src/app/components/Breadcrumbs/page.tsx
+++ src/app/components/Breadcrumbs/page.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { FaChevronRight } from "react-icons/fa6";
 type Props = {};
+import "./style.scss";
+import Link from "next/link";
 
 function Breadcrumbs({}: Props) {
   return (
-    <div>
-      <div className="route">home</div>
+    <div className="Breadcrumbs flex items-center justify-start gap-5">
+      <div className="dot rounded-full"></div>
+      <Link href={"/"} className="route">
+        home
+      </Link>
       <div className="route">
         <FaChevronRight />
       </div>
-      <div className="route">Community</div>
+      <Link href={"/community"} className="route">
+        Community
+      </Link>
     </div>
   );
 }
