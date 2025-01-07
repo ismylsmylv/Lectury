b5237ba55507c15ebc4c97ca3875f493a78a3c76
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-07 13:00:08 +0400

    feat: map added

---
+++ src/app/views/HomeView/components/ForumMap/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.scss";
+type Props = {};
+
+function ForumMap({}: Props) {
+  return <div className="ForumMap"></div>;
+}
+
+export default ForumMap;
--- src/app/views/HomeView/view/page.tsx
+++ src/app/views/HomeView/view/page.tsx
@@ -1,12 +1,16 @@
 import Breadcrumbs from "@/app/components/Breadcrumbs/page";
 import React from "react";
 import "./style.scss";
+import ForumMap from "../components/ForumMap/page";
 type Props = {};
 
 function HomeView({}: Props) {
   return (
-    <div className="HomeView p-5">
+    <div className="HomeView p-5 rounded-lg">
       <Breadcrumbs />
+      <div className="content flex gap-5">
+        <ForumMap />
+      </div>
     </div>
   );
 }
