72327198494bb91bd3bc8211316735db09ddcfac
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-07 13:22:43 +0400

    style: images added to map

--- src/app/views/HomeView/components/ForumMap/page.tsx
+++ src/app/views/HomeView/components/ForumMap/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.scss";
 import { forums } from "./mockdata";
 import { v4 as uuidv4 } from "uuid";
+import Image from "next/image";
 type Props = {};
 
 function ForumMap({}: Props) {
@@ -10,6 +11,7 @@ function ForumMap({}: Props) {
       {forums.map((forum) => (
         <div className="forum" key={uuidv4()}>
           {forum.title}
+          <Image height={50} width={50} src={forum.icon} alt="forum icon" />
         </div>
       ))}
     </div>
