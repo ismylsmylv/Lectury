b16271269b5f2ceef636ec22a3cadb3d5395f271
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-07 13:21:23 +0400

    fix: mockdata content

---
+++ src/app/views/HomeView/components/ForumMap/mockdata.ts
@@ -0,0 +1,133 @@
+import buImg from "@/app/assets/img/forumdata/bu.png";
+import ciotImg from "@/app/assets/img/forumdata/ciot.png";
+import clluImg from "@/app/assets/img/forumdata/cllu.png";
+import cmuImg from "@/app/assets/img/forumdata/cmu.png";
+import cuImg from "@/app/assets/img/forumdata/cu.png";
+import duImg from "@/app/assets/img/forumdata/du.png";
+import harvardImg from "@/app/assets/img/forumdata/harvard.png";
+import mitImg from "@/app/assets/img/forumdata/mit.png";
+import nuImg from "@/app/assets/img/forumdata/nu.png";
+import puImg from "@/app/assets/img/forumdata/pu.png";
+import stanfordImg from "@/app/assets/img/forumdata/stanford.png";
+import ucbImg from "@/app/assets/img/forumdata/ucb.png";
+import uowImg from "@/app/assets/img/forumdata/uow.png";
+import uocImg from "@/app/assets/img/forumdata/uoc.png";
+import uomImg from "@/app/assets/img/forumdata/uom.png";
+import uopImg from "@/app/assets/img/forumdata/uop.png";
+import uoscImg from "@/app/assets/img/forumdata/uosc.png";
+import yuImg from "@/app/assets/img/forumdata/yu.png";
+export const forums = [
+  // Large objects
+  {
+    title: "Harvard University Forum",
+    icon: harvardImg,
+    size: 3,
+    url: "https://www.harvard.edu/forums"
+  },
+  {
+    title: "Stanford University Forum",
+    icon: stanfordImg,
+    size: 3,
+    url: "https://www.stanford.edu/forums"
+  },
+
+  // Medium objects
+  {
+    title: "University of California, Berkeley Forum",
+    icon: ucbImg,
+    size: 2,
+    url: "https://www.berkeley.edu/forums"
+  },
+  {
+    title: "Massachusetts Institute of Technology Forum",
+    icon: mitImg,
+    size: 2,
+    url: "https://www.mit.edu/forums"
+  },
+  {
+    title: "University of Chicago Forum",
+    icon: uocImg,
+    size: 2,
+    url: "https://www.uchicago.edu/forums"
+  },
+  {
+    title: "Columbia University Forum",
+    icon: cuImg,
+    size: 2,
+    url: "https://www.columbia.edu/forums"
+  },
+  {
+    title: "Yale University Forum",
+    icon: yuImg,
+    size: 2,
+    url: "https://www.yale.edu/forums"
+  },
+
+  // Small objects
+  {
+    title: "Princeton University Forum",
+    icon: puImg,
+    size: 1,
+    url: "https://www.princeton.edu/forums"
+  },
+  {
+    title: "University of Pennsylvania Forum",
+    icon: uopImg,
+    size: 1,
+    url: "https://www.upenn.edu/forums"
+  },
+  {
+    title: "Duke University Forum",
+    icon: duImg,
+    size: 1,
+    url: "https://www.duke.edu/forums"
+  },
+  {
+    title: "University of Michigan Forum",
+    icon: uomImg,
+    size: 1,
+    url: "https://www.umich.edu/forums"
+  },
+  {
+    title: "California Institute of Technology Forum",
+    icon: ciotImg,
+    size: 1,
+    url: "https://www.caltech.edu/forums"
+  },
+  {
+    title: "Northwestern University Forum",
+    icon: nuImg,
+    size: 1,
+    url: "https://www.northwestern.edu/forums"
+  },
+  {
+    title: "Cornell University Forum",
+    icon: clluImg,
+    size: 1,
+    url: "https://www.cornell.edu/forums"
+  },
+  {
+    title: "Brown University Forum",
+    icon: buImg,
+    size: 1,
+    url: "https://www.brown.edu/forums"
+  },
+  {
+    title: "University of Southern California Forum",
+    icon: uoscImg,
+    size: 1,
+    url: "https://www.usc.edu/forums"
+  },
+  {
+    title: "Carnegie Mellon University Forum",
+    icon: cmuImg,
+    size: 1,
+    url: "https://www.cmu.edu/forums"
+  },
+  {
+    title: "University of Washington Forum",
+    icon: uowImg,
+    size: 1,
+    url: "https://www.washington.edu/forums"
+  }
+];
--- src/app/views/HomeView/components/ForumMap/page.tsx
+++ src/app/views/HomeView/components/ForumMap/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./style.scss";
+import { forums } from "./mockdata";
+import { v4 as uuidv4 } from "uuid";
 type Props = {};
 
 function ForumMap({}: Props) {
-  return <div className="ForumMap"></div>;
+  return (
+    <div className="ForumMap">
+      {forums.map((forum) => (
+        <div className="forum" key={uuidv4()}>
+          {forum.title}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ForumMap;
