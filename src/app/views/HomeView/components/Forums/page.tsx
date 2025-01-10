02144e1ea3ffa23ba178cb8204e8c7fedadbf512
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-10 15:30:44 +0400

    fix: forums structure added

--- src/app/views/HomeView/components/Forums/page.tsx
+++ src/app/views/HomeView/components/Forums/page.tsx
@@ -3,7 +3,18 @@ import "./style.scss";
 type Props = {};
 
 function Forums({}: Props) {
-  return <div className="Forums">Forums</div>;
+  return (
+    <div className="Forums">
+      <h3>Community Forums</h3>
+      <div className="types">
+        <button>all</button>
+        <button>official forums</button>
+        <button>customer forums</button>
+        <button>public forums</button>
+      </div>
+      <div className="list"></div>
+    </div>
+  );
 }
 
 export default Forums;
