77182396867f734b685296f205f49fb1c952d81d
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-11 09:26:45 +0400

    fix: mockdata added

---
+++ src/app/views/HomeView/components/Forums/mockdata.ts
@@ -0,0 +1,33 @@
+import { RiErrorWarningFill } from "react-icons/ri";
+import { FaRocket } from "react-icons/fa";
+import { FaHeadphones } from "react-icons/fa";
+export const forums = [
+  {
+    title: "Installation, Upgrade and Support",
+    bg: "#0084FF",
+    icon: FaRocket,
+    threads: 1222,
+    messages: 2467348
+  },
+  {
+    title: "Troubleshooting and Problems",
+    bg: "#70B01F",
+    icon: RiErrorWarningFill,
+    threads: 1222,
+    messages: 2467348
+  },
+  {
+    title: "Troubleshooting and Problems",
+    bg: "#7A5DEC",
+    icon: FaHeadphones,
+    threads: 1222,
+    messages: 2467348
+  },
+  {
+    title: "Troubleshooting and Problems",
+    bg: "#EE732E",
+    icon: FaRocket,
+    threads: 1222,
+    messages: 2467348
+  }
+];
