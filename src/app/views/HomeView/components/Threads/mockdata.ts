54a27741fb51dddbf69fbf8d7ce22729fd73bbf7
ismylsmylv <ismayilovsmyl2020@gmail.com>
2025-01-20 10:11:59 +0400

    style; threads ui && data updated

---
+++ src/app/views/HomeView/components/Threads/mockdata.ts
@@ -0,0 +1,31 @@
+import ProfileImg from "@/app/assets/img/profile.png";
+export const staff = [
+  {
+    displayName: "Setting up advertisement within brand new community",
+    role: "System Admin",
+    date: "Today at 6:57 PM",
+    category: "Questions and Support",
+    img: ProfileImg
+  },
+  {
+    displayName: "Setting up advertisement within brand new community",
+    role: "System Admin",
+    date: "Today at 6:57 PM",
+    category: "Questions and Support",
+    img: ""
+  },
+  {
+    displayName: "Setting up advertisement within brand new community",
+    role: "System Admin",
+    date: "Today at 6:57 PM",
+    category: "Questions and Support",
+    img: ProfileImg
+  },
+  {
+    displayName: "Setting up advertisement within brand new community",
+    date: "Today at 6:57 PM",
+    category: "Questions and Support",
+    role: "System Admin",
+    img: ""
+  }
+];
--- src/app/views/HomeView/components/Threads/page.tsx
+++ src/app/views/HomeView/components/Threads/page.tsx
@@ -1,9 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
 import React from "react";
 import "./style.scss";
+
+import { staff } from "./mockdata";
+
+import { FiUsers } from "react-icons/fi";
+
+import { FaUser } from "react-icons/fa6";
+import Image from "next/image";
+import Link from "next/link";
+import HeadingSm from "@/app/components/HeadingSm/page";
+
 type Props = {};
 
 function Threads({}: Props) {
-  return <div className="Threads">Threads</div>;
+  const headData = { text: "staff online", icon: FiUsers };
+  return (
+    <div className="Threads mb-15">
+      <HeadingSm heading={headData} />
+      <div className="staff">
+        {staff.map((member) => (
+          <Link
+            href={"#"}
+            key={uuidv4()}
+            className="member flex items-center justify-start gap-3 mb-3 pr-3  rounded-lg "
+          >
+            <div className="img rounded-lg flex items-center justify-center">
+              {member.img ? (
+                <Image alt="profile picture" src={member.img} />
+              ) : (
+                <FaUser />
+              )}
+            </div>
+            <div className="info opacity-70 hover:opacity-100">
+              <div className="name font-medium line-clamp-1">
+                {member.displayName}
+              </div>
+              <div className="role font-light line-clamp-1"> {member.role}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="line mt-3"></div>
+    </div>
+  );
 }
 
 export default Threads;
